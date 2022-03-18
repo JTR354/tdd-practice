ead4a54d59e3fe7fa477911249e7d7631cb51218
jtr354 <jtr354@foxmail.com>
2022-03-18 18:02:40 +0800

    feat: args happy path

--- src/args/index.js
+++ src/args/index.js
@@ -1,3 +1,21 @@
-export function add(a, b) {
-  return a + b;
+export default function parseArgs(str) {
+  let result = {};
+  str.split(" ").forEach((it, index, arr) => {
+    result = Object.assign(result, parse(it, index, arr));
+  });
+  return result;
+}
+
+function parse(it, index, arr) {
+  if (it.startsWith("-")) {
+    if (it[1] === "l") {
+      return { logging: true };
+    }
+    if (it[1] === "p") {
+      return { port: Number.parseInt(arr[index + 1]) };
+    }
+    if (it[1] === "d") {
+      return { directory: String(arr[index + 1]) };
+    }
+  }
 }
--- src/args/tests/index.spec.js
+++ src/args/tests/index.spec.js
@@ -1,26 +1,36 @@
+import parseArgs from "../index";
 describe("args", () => {
   /**
    * “d”（目录）有一个字符串值。标志后面如果存在多个值，则该标志表示一个列表：
    * -g this is a list -d 1 2 -3 5　"g"表示一个字符串列表[“this”, “is”, “a”, “list”]，“d"标志表示一个整数列表[1, 2, -3, 5]。
    */
   // single:
-  //  - bool: -l
-  //  - int: -p 8080
-  //  - string: -d /usr/logs
-  // multi: -l -p 8080 -d /usr/logs
+  //TODO  - bool: -l
+  test("parse -l is true", () => {
+    expect(parseArgs("-l")).toEqual({ logging: true });
+  });
+  //TODO  - int: -p 8080
+  test("parse -p is 8080", () => {
+    expect(parseArgs("-p 8080")).toEqual({ port: 8080 });
+  });
+  //TODO  - string: -d /usr/logs
+  test("parse -d is /usr/logs", () => {
+    expect(parseArgs("-d /usr/logs")).toEqual({ directory: "/usr/logs" });
+  });
+  //TODO multi: -l -p 8080 -d /usr/logs
   // sad path:
-  //  - bool: -l t/ -l t f
-  //  - int: -p 8080 8081
-  //  - string: -d /usr/logs /user/chat
+  //TODO  - bool: -l t/ -l t f
+  //TODO  - int: -p 8080 8081
+  //TODO  - string: -d /usr/logs /user/chat
   // default:
-  // - bool: false
-  // - int: 0
-  // - string: ''
-  test.skip("demo1", () => {
+  //TODO - bool: false
+  //TODO - int: 0
+  //TODO - string: ''
+  test("demo1", () => {
     expect(parseArgs("-l -p 8080 -d /usr/logs")).toEqual({
-      l: true,
-      p: 8080,
-      d: "/usr/logs",
+      logging: true,
+      port: 8080,
+      directory: "/usr/logs",
     });
   });
   test.skip("should 2", () => {
