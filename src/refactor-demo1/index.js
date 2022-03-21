557947005a361fa621777a615a0006a9d1720b35
mi <jtr354@foxmail.com>
2022-03-21 23:16:29 +0800

    feat: 重构demo1基本完p35

--- src/refactor-demo1/index.js
+++ src/refactor-demo1/index.js
@@ -1,3 +1,4 @@
+import createStatementData from "./createStateData";
 export const invoice = [
   {
     customer: "BigCo",
@@ -25,56 +26,185 @@ export const plays = {
 };
 
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
+  return renderPlainText(createStatementData(invoice, plays));
+
+  // function createStatementData(invoice, plays) {
+  //   const statementData = {};
+  //   statementData.customer = invoice.customer;
+  //   statementData.performances = invoice.performances.map(enrichPerformance);
+  //   statementData.totalAmount = totalAmount(statementData);
+  //   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  //   return statementData;
+  // }
+
+  // function totalAmount({performances}) {
+  //   // let result = 0;
+  //   // for (let perf of data.performances) {
+  //   //   result += perf.amount;
+  //   // }
+  //   return performances.reduce((total, p) => total + p.amount, 0);
+  // }
+
+  // function totalVolumeCredits({performances}) {
+  //   // let result = 0;
+  //   // for (let perf of data.performances) {
+  //   //   result += perf.volumeCredits;
+  //   // }
+  //   return performances.reduce((total, p) => total + p.volumeCredits, 0);
+  // }
+
+  // function enrichPerformance(aPerformance) {
+  //   const result = Object.assign({}, aPerformance)
+  //   result.play = playFor(result)
+  //   result.amount = amountFor(result)
+  //   result.volumeCredits = volumeCreditsFor(result)
+  //   return result
+  // }
+
+  // function playFor(aPerformance) {
+  //   return plays[aPerformance.playID];
+  // }
+  // function amountFor(aPerformance) {
+  //   let result = 0;
+
+  //   switch (aPerformance.play.type) {
+  //     case "tragedy":
+  //       result = 40000;
+  //       if (aPerformance.audience > 30) {
+  //         result += 1000 * (aPerformance.audience - 30);
+  //       }
+  //       break;
+  //     case "comedy":
+  //       result = 30000;
+  //       if (aPerformance.audience > 20) {
+  //         result += 10000 + 500 * (aPerformance.audience - 20);
+  //       }
+  //       result += 300 * aPerformance.audience;
+  //       break;
+  //     default:
+  //       throw new Error(`unknown type:${aPerformance.play.type}`);
+  //   }
+  //   return result;
+  // }
+  // function volumeCreditsFor(aPerformance) {
+  //   let result = 0;
+  //   // add volume credits
+  //   result += Math.max(aPerformance.audience - 30, 0);
+  //   // add extra credit for every ten comedy attendees
+  //   if ("comedy" === aPerformance.play.type) {
+  //     result += Math.floor(aPerformance.audience / 5);
+  //   }
+  //   return result;
+  // }
+}
+
+function renderPlainText(data) {
+  // let totalAmount = 0;
+  // let volumeCredits = 0;
+  let result = `Statement for ${data.customer}\n`;
+  // const format = format();
+
+  for (let perf of data.performances) {
+    // const play = playFor(perf); *** 移除play
+    // let thisAmount = amountFor(perf); *** 移除thisAmount
+    // 提炼计算观众量积分的逻辑 ***
+    // volumeCredits += volumeCreditsFor(perf);
+    // print line for this order
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`;
+    // totalAmount += amountFor(perf);
+  }
+  // let totalAmount = appleSauce();
+  // 移除积分计算 ***
+  // let volumeCredits = totalVolumeCredits();
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  return result;
+
+  // function totalAmount() {
+  //   let result = 0;
+  //   for (let perf of data.performances) {
+  //     result += perf.amount;
+  //   }
+  //   return result;
+  // }
+
+  // function totalVolumeCredits() {
+  //   let result = 0;
+  //   for (let perf of data.performances) {
+  //     result += perf.volumeCredits;
+  //   }
+  //   return result;
+  // }
+
+  // function volumeCreditsFor(aPerformance) {
+  //   let result = 0;
+  //   // add volume credits
+  //   result += Math.max(aPerformance.audience - 30, 0);
+  //   // add extra credit for every ten comedy attendees
+  //   if ("comedy" === aPerformance.play.type) {
+  //     result += Math.floor(aPerformance.audience / 5);
+  //   }
+  //   return result;
+  // }
+
+  /**移除play变量 */
+  // function aPerformance.play {
+  //   return plays[aPerformance.playID];
+  // }
+
+  /**关注点分离 switch */
+  // function amountFor(aPerformance) {
+  //   let result = 0;
+
+  //   switch (aPerformance.play.type) {
+  //     case "tragedy":
+  //       result = 40000;
+  //       if (aPerformance.audience > 30) {
+  //         result += 1000 * (aPerformance.audience - 30);
+  //       }
+  //       break;
+  //     case "comedy":
+  //       result = 30000;
+  //       if (aPerformance.audience > 20) {
+  //         result += 10000 + 500 * (aPerformance.audience - 20);
+  //       }
+  //       result += 300 * aPerformance.audience;
+  //       break;
+  //     default:
+  //       throw new Error(`unknown type:${aPerformance.play.type}`);
+  //   }
+  //   return result;
+  // }
+}
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
-  }).format;
-
-  for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    } seats)\n`;
-    totalAmount += thisAmount;
-  }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
-
-  function playFor(perf) {
-    return plays[perf.playID];
-  }
+  }).format(aNumber / 100);
 }
-/**关注点分离 switch */
-function amountFor(aPerformance, play) {
-  let result = 0;
 
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type:${play.type}`);
+export function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>`;
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${
+      perf.audience
+    }</td><td>${usd(perf.amount)}</td></tr>\n`;
+    // totalAmount += amountFor(perf);
   }
+  result += `</table>`;
+  // let totalAmount = appleSauce();
+  // 移除积分计算 ***
+  // let volumeCredits = totalVolumeCredits();
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
   return result;
 }
--- src/refactor-demo1/index.spec.js
+++ src/refactor-demo1/index.spec.js
@@ -1,4 +1,4 @@
-import { statement, invoice, plays } from "./index";
+import { statement, htmlStatement, invoice, plays } from "./index";
 describe("重构，第一个示例", () => {
   test("demo1", () => {
     expect(statement(invoice[0], plays)).toEqual(
@@ -11,4 +11,16 @@ You earned 47 credits
 `
     );
   });
+  test('demo2', () => {
+    expect(htmlStatement(invoice[0], plays)).toEqual(
+      `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>cost</th></tr> <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+ <tr><td>As You Like it</td><td>35</td><td>$580.00</td></tr>
+ <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table><p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`
+    )
+  })
 });
