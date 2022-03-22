e288f020fa869b7acd9baea3bb753c833047e912
jtr354 <jtr354@foxmail.com>
2022-03-22 18:24:57 +0800

    feat: [vitest] init & setup"

--- src/args/tests/index.spec.js
+++ src/args/tests/index.spec.js
@@ -1,3 +1,5 @@
+import { describe, test, expect } from "vitest";
+
 import parseArgs from "../index";
 describe("args", () => {
   /**
--- src/refactor-demo1/index.spec.js
+++ src/refactor-demo1/index.spec.js
@@ -1,3 +1,4 @@
+import { describe, expect, test } from "vitest";
 import { statement, htmlStatement } from "./index";
 import { invoice, plays } from "./data";
 
